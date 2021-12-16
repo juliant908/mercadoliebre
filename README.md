58f01284304bdba27d518012b70ab279c755f526
juliant908 <juliant908@gmail.com>
2021-12-16 14:40:03 -0500

    Uploading project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Proyecto de MercadoLiebre con Digital House
+
+## Proyecto individual de la cursada de Full Stack Development en Digital house
+
+Este es un proyecto individual donde crearemos una réplica responsive de MercadoLibre, para poner en práctica los conocimientos en HTML, CSS, NodeJS y ExpressJS.
+
+Desarrollado por: Julián Tibana
+
+Full Stack Developer.
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicpath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicpath));
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/home.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/register.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/login.html"))
+);
+app.post("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/login.html"))
+);
+app.post("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/home.html"))
+);
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
